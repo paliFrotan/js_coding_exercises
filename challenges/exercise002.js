b9a9fd0615083d087388c43ccda3859cae5ac3af
Pali Frotan <pali.frotan@icloud.com>
2023-05-18 16:38:52 +0100

    Feat:Exercise002 completed

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,24 @@
 export function getFillings(sandwich) {
 	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+	return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
 	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+	return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	return arr.filter(x => x === "sheep").length;
 }
 
 export function hasMPostCode(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	return person.address.postCode.substring(0, 1) === "M" && person.address.city === "Manchester";
 }
